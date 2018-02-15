813d3d4e4a9d591bed013c3fb58a24d9c49c47c8
saki <sakihet@gmail.com>
2018-02-16 02:32:43 +0900

    refactor

--- node/puppeteer-screenshot.js
+++ node/puppeteer-screenshot.js
@@ -2,14 +2,16 @@ const puppeteer = require('puppeteer')
 
 let url = process.argv[2]
 
+async function screenshot(url) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({ path: `screenshot.png` })
+  await browser.close()
+}
+
 if (url) {
-  ;(async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
-    await page.screenshot({ path: `screenshot.png` })
-    await browser.close()
-  })()
+  screenshot(url)
 } else {
   console.log('url is missing')
 }
