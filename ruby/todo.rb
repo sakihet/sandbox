51711c20f98066fcdd70223c27507baa08e0a914
saki <sakihet@gmail.com>
2018-03-20 09:01:40 +0900

    refactor todo.rb

--- ruby/todo.rb
+++ ruby/todo.rb
@@ -2,43 +2,57 @@ require 'fileutils'
 require 'json'
 
 TODO_FILE_PATH = './ruby/todo.txt'
-
+Task = Struct.new(:name, :is_completed)
 command = ARGV[0]
 rest = ARGV[1..-1]
 tasks = []
 
+if !File.exists? TODO_FILE_PATH
+  File.open(TODO_FILE_PATH, 'w')
+end
+
+def read_json(path)
+  JSON.parse(File.read(path))
+rescue
+  []
+end
+
+tasks = read_json(TODO_FILE_PATH).map {|v|
+  Task.new(v["name"], v["is_completed"])
+}
+
+def write_json(path, array_task)
+  File.open(path, 'w') do |f|
+    data = array_task.map {|v|
+      v.to_h
+    }.to_json
+    f.write(data)
+  end
+end
+
 case command
 when 'list'
-  tasks = JSON.parse(File.read(TODO_FILE_PATH))
+  begin
   tasks.each_with_index do |task, i|
-    checkbox = "[ ]"
-    checkbox = "[x]" if task["is_completed"]
-    name = task["name"]
-    puts "#{i}: #{checkbox} #{name}"
+    checkbox = task.is_completed ? "[x]" : "[ ]"
+    puts "#{i}: #{checkbox} #{task.name}"
+  end
+  rescue
+    puts 'no tasks'
   end
 when 'add'
-  new_task = {
-    is_completed: false,
-    name: rest.join(' ').to_s
-  }
-  tasks = JSON.parse(File.read(TODO_FILE_PATH))
-  File.open(TODO_FILE_PATH, 'w') do |f|
-    f.write(tasks.push(new_task).to_json)
-  end
+  name = rest.join(' ').to_s
+  new_task = Task.new(name, false)
+  tasks.push(new_task)
+  write_json(TODO_FILE_PATH, tasks)
 when 'delete'
   id = rest[0].to_i
-  tasks = JSON.parse(File.read(TODO_FILE_PATH))
   tasks.delete_at(id)
-  File.open(TODO_FILE_PATH, 'w') do |f|
-    f.write(tasks.to_json)
-  end
+  write_json(TODO_FILE_PATH, tasks)
 when 'done'
   id = rest[0].to_i
-  tasks = JSON.parse(File.read(TODO_FILE_PATH))
-  tasks[id]["is_completed"] = true
-  File.open(TODO_FILE_PATH, 'w') do |f|
-    f.write(tasks.to_json)
-  end
+  tasks[id].is_completed = true
+  write_json(TODO_FILE_PATH, tasks)
 else
   puts 'no command'
 end
