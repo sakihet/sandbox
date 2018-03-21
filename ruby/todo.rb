53cb4c4a6960a050224ff094667541814a95b482
saki <sakihet@gmail.com>
2018-03-21 15:55:00 +0900

    refine todo.rb

--- ruby/todo.rb
+++ ruby/todo.rb
@@ -1,8 +1,15 @@
+require 'date'
 require 'fileutils'
 require 'json'
 
 TODO_FILE_PATH = './ruby/todo.txt'
-Task = Struct.new(:name, :is_completed)
+Task = Struct.new(
+  :name,
+  :is_completed,
+  :created,
+  :updated
+)
+
 command = ARGV[0]
 rest = ARGV[1..-1]
 tasks = []
@@ -18,7 +25,12 @@ rescue
 end
 
 tasks = read_json(TODO_FILE_PATH).map {|v|
-  Task.new(v["name"], v["is_completed"])
+  Task.new(
+    v["name"],
+    v["is_completed"],
+    v["created"],
+    v["updated"]
+  )
 }
 
 def write_json(path, array_task)
@@ -35,14 +47,20 @@ when 'list'
   begin
   tasks.each_with_index do |task, i|
     checkbox = task.is_completed ? "[x]" : "[ ]"
-    puts "#{i}: #{checkbox} #{task.name}"
+    printf "%2d: #{checkbox} #{task.name} [#{task.updated}]\n", i
   end
   rescue
     puts 'no tasks'
   end
 when 'add'
   name = rest.join(' ').to_s
-  new_task = Task.new(name, false)
+  now_str = DateTime.now.to_s
+  new_task = Task.new(
+    name,
+    false,
+    now_str,
+    now_str
+  )
   tasks.push(new_task)
   write_json(TODO_FILE_PATH, tasks)
 when 'delete'
@@ -52,7 +70,13 @@ when 'delete'
 when 'done'
   id = rest[0].to_i
   tasks[id].is_completed = true
+  tasks[id].updated = DateTime.now.to_s
   write_json(TODO_FILE_PATH, tasks)
+when 'progress'
+  done_tasks = tasks.select {|t| t.is_completed == true}
+  progress = (done_tasks.size.to_f / tasks.size * 100).round
+  puts "#{done_tasks.size} / #{tasks.size}"
+  puts "#{progress} %"
 else
   puts 'no command'
 end
