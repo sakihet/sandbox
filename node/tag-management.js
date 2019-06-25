d939744ebf250fde7ca097b795e26ac067be0649
saki <sakihet@gmail.com>
2019-06-26 01:17:26 +0900

    add tag-management.js

---
+++ node/tag-management.js
@@ -0,0 +1,46 @@
+let ary = [
+  "tag1",
+  " tag1 ",
+  "tag1,tag2",
+  " tag1, tag3",
+  " tag2 ,, tag4",
+  "tag5 tag2 "
+]
+
+let tags = []
+
+tagParser = (str) => {
+  return str.trim().replace(/ /g, ',').split(',').filter(e => e)
+}
+
+addTags = (names) => {
+  let ids = []
+  names.forEach(name => {
+    if ( tags.filter(x => x.name === name).length >= 1 ) {
+      console.log('same tag exists in tags')
+      ids.push(tags.find(x => x.name === name).id)
+    } else {
+      console.log('new tag')
+      let newId
+      if (0 < tags.length) {
+        newId = tags[tags.length - 1].id + 1
+      } else {
+        newId = 1
+      }
+      let tag = { id: newId, name: name }
+      tags.push(tag)
+      console.log('tags:')
+      console.log(tags)
+      ids.push(tag.id)
+    }
+  })
+  console.log('ids:')
+  console.log(ids)
+}
+
+ary.forEach(str => {
+  console.log('input string: ', str)
+  let tags = tagParser(str)
+  console.log('parsed tags: ', tags)
+  addTags(tags)
+})
