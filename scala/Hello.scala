5d9420712ccd0c3f08100d7e748af4de743d5fa2
saki <sakihet@gmail.com>
2020-09-14 23:38:28 +0900

    add Point class

--- scala/Hello.scala
+++ scala/Hello.scala
@@ -1,5 +1,21 @@
+case class Point(_x: Int, _y: Int) {
+  val x = _x
+  val y = _y
+
+  def +(p: Point): Point = {
+    new Point(x + p.x, y + p.y)
+  }
+
+  override def toString(): String = "(" + x + ", " + y + ")"
+}
+
 object Hello {
   def main(args: Array[String]) = {
     println("hello, world")
+    val p1 = new Point(1, 2)
+    val p2 = new Point(2, 3)
+    println(p1)
+    println(p2)
+    println(p1 + p2)
   }
 }
