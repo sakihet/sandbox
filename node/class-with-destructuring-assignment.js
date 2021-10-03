23b525b0239d810d31d1020af1a396fe199516e9
saki <sakihet@gmail.com>
2021-10-03 11:12:16 +0900

    add class-with-destructuring-assignment.js

---
+++ node/class-with-destructuring-assignment.js
@@ -0,0 +1,23 @@
+class ValidationError extends Error {}
+
+class Person {
+  constructor(
+    {
+      id,
+      name,
+      gender = null
+    } = {}
+  ) {
+    if (id === undefined || name === undefined) {
+      throw new ValidationError()
+    }
+    this.id = id
+    this.name = name
+    this.gender = gender
+  }
+}
+const p1 = new Person({ id: 1, name: 'adam', gender: 'male' })
+const obj = { id: 2, name: 'bob' }
+const p2 = new Person(obj)
+console.log(p1)
+console.log(p2)
