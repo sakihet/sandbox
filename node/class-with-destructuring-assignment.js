ac3ea8dce16cadf25222c3025ba41cbc0b72591f
saki <sakihet@gmail.com>
2021-10-07 23:54:54 +0900

    update class-with-destructuring-assignment.js

--- node/class-with-destructuring-assignment.js
+++ node/class-with-destructuring-assignment.js
@@ -15,9 +15,14 @@ class Person {
     this.name = name
     this.gender = gender
   }
+  sayName () {
+    console.log(this.name)
+  }
 }
 const p1 = new Person({ id: 1, name: 'adam', gender: 'male' })
 const obj = { id: 2, name: 'bob' }
 const p2 = new Person(obj)
 console.log(p1)
 console.log(p2)
+
+new Person(obj).sayName()
